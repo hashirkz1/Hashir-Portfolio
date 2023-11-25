d6d947a40b4c63a7628087d115c6e1509c2979a8
Hashir Rizwan <mhashirrizwan@gmail.com>
2023-11-24 23:51:31 -0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+let x = () =>{
+    location.href = "https://github.com/hashirkz1"
+}
+let y = () =>{
+    location.href = "\\Muhammad-Hashir-Rizwan.pdf"
+}
+let id1 = document.getElementById("id1")
+id1.addEventListener("click",y)
+let id2 = document.getElementById("id2")
+id2.addEventListener("click",x)
